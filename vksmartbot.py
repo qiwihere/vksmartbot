82a595902c53b3122c4fee4ec8e34b28535e8f3e
Артем <qiwihere@gmail.com>
2019-01-10 02:35:25 +0300

    Update

--- vksmartbot.py
+++ vksmartbot.py
@@ -1,51 +1,58 @@
 import vk_api
 import random
 import requests
-from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 
 API_KEY = '386f42c8e90decc0b69903e39ff2b3df71fb2afb42e5463df3830e77097532ad707228314279a726a90c1'
 
 vk_session = vk_api.VkApi(token=API_KEY)
 
-longpoll = VkLongPoll(vk_session)
+longpoll = VkBotLongPoll(vk_session, 'your_group_id')
 
-vk = vk_session.get_api()
-for event in longpoll.listen():
+    for event in longpoll.listen():
 
-        if event.type == VkEventType.MESSAGE_NEW:
+        if event.type == VkBotEventType.MESSAGE_NEW:
             print('Новое сообщение:')
 
-            if event.from_me:
-                print('От меня для: ', end='')
-            elif event.to_me:
-                print('Для меня от: ', end='')
+            print('Для меня от: ', end='')
 
-            if event.from_user:
-                print(event.user_id)
-            elif event.from_chat:
-                print(event.user_id, 'в беседе', event.chat_id)
-            elif event.from_group:
-                print('группы', event.group_id)
+            print(event.obj.from_id)
 
-            print('Текст: ', event.text)
+            print('Текст:', event.obj.text)
             print()
 
-        elif event.type == VkEventType.USER_TYPING:
+        elif event.type == VkBotEventType.MESSAGE_REPLY:
+            print('Новое сообщение:')
+
+            print('От меня для: ', end='')
+
+            print(event.obj.peer_id)
+
+            print('Текст:', event.obj.text)
+            print()
+
+        elif event.type == VkBotEventType.MESSAGE_TYPING_STATE:
             print('Печатает ', end='')
 
-            if event.from_user:
-                print(event.user_id)
-            elif event.from_group:
-                print('администратор группы', event.group_id)
+            print(event.obj.from_id, end=' ')
 
-        elif event.type == VkEventType.USER_TYPING_IN_CHAT:
-            print('Печатает ', event.user_id, 'в беседе', event.chat_id)
+            print('для ', end='')
 
-        elif event.type == VkEventType.USER_ONLINE:
-            print('Пользователь', event.user_id, 'онлайн', event.platform)
+            print(event.obj.to_id)
+            print()
 
-        elif event.type == VkEventType.USER_OFFLINE:
-            print('Пользователь', event.user_id, 'оффлайн', event.offline_type)
+        elif event.type == VkBotEventType.GROUP_JOIN:
+            print(event.obj.user_id, end=' ')
+
+            print('Вступил в группу!')
+            print()
+
+        elif event.type == VkBotEventType.GROUP_LEAVE:
+            print(event.obj.user_id, end=' ')
+
+            print('Покинул группу!')
+            print()
 
         else:
-            print(event.type, event.raw[1:])
+            print(event.type)
+            print()
