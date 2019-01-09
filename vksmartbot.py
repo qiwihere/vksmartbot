f838c470aa28da059e36909e3438cc0e12f9d558
Артем <qiwihere@gmail.com>
2019-01-10 02:04:46 +0300

    Update

--- vksmartbot.py
+++ vksmartbot.py
@@ -6,21 +6,14 @@ API_KEY = '386f42c8e90decc0b69903e39ff2b3df71fb2afb42e5463df3830e77097532ad70722
 
 vk_session = vk_api.VkApi(token=API_KEY)
 
-from vk_api.longpoll import VkLongPoll, VkEventType
-
 longpoll = VkLongPoll(vk_session)
+print('test')
 vk = vk_session.get_api()
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
-   #Слушаем longpoll, если пришло сообщение то:
-        if event.text == 'Первый вариант фразы' or event.text == 'Второй вариант фразы': #Если написали заданную фразу
-            if event.from_user: #Если написали в ЛС
-                vk.messages.send( #Отправляем сообщение
-                    user_id=event.user_id,
-                    message='Ваш текст'
-		        )
-            elif event.from_chat: #Если написали в Беседе
-                vk.messages.send( #Отправляем собщение
-                    chat_id=event.chat_id,
-                    message='Ваш текст'
-		        )
+
+        if event.text == '1':
+            if event.from_user:
+                vk.messages.send(user_id=event.user_id, message='Ваш текст')
+            elif event.from_chat:
+                vk.messages.send(chat_id=event.chat_id, message='Ваш текст')
