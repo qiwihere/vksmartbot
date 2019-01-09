19d38eb969ac1b6e277075058b23f52ccc26d2e9
Артем <qiwihere@gmail.com>
2019-01-10 02:15:56 +0300

    Update

--- vksmartbot.py
+++ vksmartbot.py
@@ -7,13 +7,13 @@ API_KEY = '386f42c8e90decc0b69903e39ff2b3df71fb2afb42e5463df3830e77097532ad70722
 vk_session = vk_api.VkApi(token=API_KEY)
 
 longpoll = VkLongPoll(vk_session)
-print('test')
+
 vk = vk_session.get_api()
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
 
         if event.text == '1':
             if event.from_user:
-                vk.messages.send(user_id=event.user_id, message='Ваш текст')
+                vk.messages.send(user_id=event.user_id, random_id=event.random_id, message='Ваш текст')
             elif event.from_chat:
-                vk.messages.send(chat_id=event.chat_id, message='Ваш текст')
+                vk.messages.send(chat_id=event.chat_id, random_id=event.random_id, message='Ваш текст')
