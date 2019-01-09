212b328f41e1524f405a8e88b6ce838ee7435de1
Артем <qiwihere@gmail.com>
2019-01-10 02:30:58 +0300

    Update

--- vksmartbot.py
+++ vksmartbot.py
@@ -11,10 +11,41 @@ longpoll = VkLongPoll(vk_session)
 
 vk = vk_session.get_api()
 for event in longpoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
 
-        if event.text == 'Кто такой Данчос':
+        if event.type == VkEventType.MESSAGE_NEW:
+            print('Новое сообщение:')
+
+            if event.from_me:
+                print('От меня для: ', end='')
+            elif event.to_me:
+                print('Для меня от: ', end='')
+
             if event.from_user:
-                vk.messages.send(user_id=event.user_id, random_id=random.randint(0, 100000), message='Самый лучший музыкант')
+                print(event.user_id)
             elif event.from_chat:
-                vk.messages.send(chat_id=event.chat_id, random_id=random.randint(0, 100000), message='Самый лучший музыкант')
+                print(event.user_id, 'в беседе', event.chat_id)
+            elif event.from_group:
+                print('группы', event.group_id)
+
+            print('Текст: ', event.text)
+            print()
+
+        elif event.type == VkEventType.USER_TYPING:
+            print('Печатает ', end='')
+
+            if event.from_user:
+                print(event.user_id)
+            elif event.from_group:
+                print('администратор группы', event.group_id)
+
+        elif event.type == VkEventType.USER_TYPING_IN_CHAT:
+            print('Печатает ', event.user_id, 'в беседе', event.chat_id)
+
+        elif event.type == VkEventType.USER_ONLINE:
+            print('Пользователь', event.user_id, 'онлайн', event.platform)
+
+        elif event.type == VkEventType.USER_OFFLINE:
+            print('Пользователь', event.user_id, 'оффлайн', event.offline_type)
+
+        else:
+            print(event.type, event.raw[1:])
