76ca3560927c37c68513546880fef66ede2651ab
Артем <qiwihere@gmail.com>
2019-01-10 13:56:31 +0300

    Update

--- vksmartbot.py
+++ vksmartbot.py
@@ -1,6 +1,5 @@
 import vk_api
 import random
-import requests
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 
 API_KEY = '386f42c8e90decc0b69903e39ff2b3df71fb2afb42e5463df3830e77097532ad707228314279a726a90c1'
@@ -17,7 +16,7 @@ for event in longpoll.listen():
                 if event.obj.text:
                     vk.messages.send(
                         user_id=event.obj.from_id,
-                        random_id=random.randint(0, 10 ^ 24),
+                        random_id=event.random_id,
                         message=event.obj.text
                     )
                 print(event.obj.attachments)
