80d96d6d6ef8d33cfde40752767b639d0e394ec5
Артем <qiwihere@gmail.com>
2019-01-10 13:24:56 +0300

    Update

--- vksmartbot.py
+++ vksmartbot.py
@@ -6,54 +6,23 @@ from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 API_KEY = '386f42c8e90decc0b69903e39ff2b3df71fb2afb42e5463df3830e77097532ad707228314279a726a90c1'
 
 vk_session = vk_api.VkApi(token=API_KEY)
-
+vk = vk_session.get_api()
 
 longpoll = VkBotLongPoll(vk_session, '176461659')
 
 for event in longpoll.listen():
 
         if event.type == VkBotEventType.MESSAGE_NEW:
-            print('Новое сообщение:')
+            if event.from_user:  # Если написали в ЛС
+                vk.messages.send(  # Отправляем сообщение
+                    user_id=event.user_id,
+                    random_id=random.randint(0,1000),
+                    message='Ваш текст'
+                )
+            elif event.from_chat:  # Если написали в Беседе
+                vk.messages.send(  # Отправляем собщение
+                    chat_id=event.chat_id,
+                    random_id=random.randint(0,1000),
+                    message='Ваш текст'
+                )
 
-            print('Для меня от: ', end='')
-
-            print(event.obj.from_id)
-
-            print('Текст:', event.obj.text)
-            print()
-
-        elif event.type == VkBotEventType.MESSAGE_REPLY:
-            print('Новое сообщение:')
-
-            print('От меня для: ', end='')
-
-            print(event.obj.peer_id)
-
-            print('Текст:', event.obj.text)
-            print()
-
-        elif event.type == VkBotEventType.MESSAGE_TYPING_STATE:
-            print('Печатает ', end='')
-
-            print(event.obj.from_id, end=' ')
-
-            print('для ', end='')
-
-            print(event.obj.to_id)
-            print()
-
-        elif event.type == VkBotEventType.GROUP_JOIN:
-            print(event.obj.user_id, end=' ')
-
-            print('Вступил в группу!')
-            print()
-
-        elif event.type == VkBotEventType.GROUP_LEAVE:
-            print(event.obj.user_id, end=' ')
-
-            print('Покинул группу!')
-            print()
-
-        else:
-            print(event.type)
-            print()
