e7039dd8c4e19846c554ed06fdc9f48e4efb86a5
Артем <qiwihere@gmail.com>
2019-01-10 14:08:03 +0300

    Update

--- vksmartbot.py
+++ vksmartbot.py
@@ -1,5 +1,5 @@
 import vk_api
-import random
+import json
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 
 API_KEY = '386f42c8e90decc0b69903e39ff2b3df71fb2afb42e5463df3830e77097532ad707228314279a726a90c1'
@@ -9,21 +9,34 @@ vk = vk_session.get_api()
 
 longpoll = VkBotLongPoll(vk_session, '176461659')
 
+
+def parse_voice(event):
+    if event.attachments:
+        attachments = json.loads(event.attachments).pop()
+        if attachments.type == 'audio_message':
+            return attachments.audio_message.link_ogg
+
+
 for event in longpoll.listen():
 
         if event.type == VkBotEventType.MESSAGE_NEW:
             if event.from_user:
+                print(parse_voice(event))
+                '''
                 if event.obj.text:
                     vk.messages.send(
                         user_id=event.obj.from_id,
                         random_id=event.obj.random_id,
                         message=event.obj.text
                     )
-                print(event.obj.attachments)
+                '''
+
             if event.from_chat:
+                '''
                 vk.messages.send(
                     chat_id=event.chat_id,
-                    random_id=random.randint(0, 10 ^ 24),
+                    random_id=event.obj.random_id,
                     message=event.obj.text
                 )
+                '''
 
